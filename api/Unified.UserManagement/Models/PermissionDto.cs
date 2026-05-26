f183a81ebcc05ee59419f316310318f93d0f9fc0
Hemanth Kona <hemanth.kona@quartech.com>
2026-05-26 12:16:23 -0400

    format api

--- api/Unified.UserManagement/Models/PermissionDto.cs
+++ api/Unified.UserManagement/Models/PermissionDto.cs
@@ -5,8 +5,8 @@ public sealed record PermissionDto
     public string Id { get; init; } = string.Empty;
 
     public string Description { get; init; } = string.Empty;
-    
+
     public string Group { get; init; } = string.Empty;
-    
+
     public uint ConcurrencyToken { get; init; }
 }
