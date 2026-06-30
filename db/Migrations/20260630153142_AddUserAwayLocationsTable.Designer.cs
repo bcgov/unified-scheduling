764a941ff053e9b41a619cd8b608bcf2417330ff
Hemanth Kona <hemanth.kona@quartech.com>
2026-06-30 12:45:11 -0400

    SS-923: Setup UserAwayLocation Data model
    - generate UserAwayLocation migrations

--- db/Migrations/20260630153142_AddUserAwayLocationsTable.Designer.cs
+++ db/Migrations/20260630153142_AddUserAwayLocationsTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Unified.Db;
@@ -11,9 +12,11 @@ using Unified.Db;
 namespace Unified.Db.Migrations
 {
     [DbContext(typeof(UnifiedDbContext))]
-    partial class UnifiedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260630153142_AddUserAwayLocationsTable")]
+    partial class AddUserAwayLocationsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -980,12 +983,6 @@ namespace Unified.Db.Migrations
                         .HasMaxLength(200)
                         .HasColumnType("character varying(200)");
 
-                    b.Property<DateTimeOffset>("EffectiveDate")
-                        .HasColumnType("timestamp with time zone");
-
-                    b.Property<DateTimeOffset?>("ExpiryDate")
-                        .HasColumnType("timestamp with time zone");
-
                     b.Property<bool>("Mandatory")
                         .HasColumnType("boolean");
 
@@ -995,7 +992,7 @@ namespace Unified.Db.Migrations
                     b.Property<bool>("Rotating")
                         .HasColumnType("boolean");
 
-                    b.Property<int?>("TrainingCategoryId")
+                    b.Property<int>("TrainingCategoryId")
                         .HasColumnType("integer");
 
                     b.Property<Guid?>("UpdatedById")
@@ -1345,17 +1342,6 @@ namespace Unified.Db.Migrations
                         .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<DateTimeOffset?>("LastPhotoUpdate")
-                        .HasColumnType("timestamp with time zone");
-
-                    b.Property<bool>("PendingRegistration")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("boolean")
-                        .HasDefaultValue(false);
-
-                    b.Property<byte[]>("Photo")
-                        .HasColumnType("bytea");
-
                     b.Property<string>("Rank")
                         .HasColumnType("text");
 
@@ -1370,15 +1356,6 @@ namespace Unified.Db.Migrations
                     b.HasIndex("CreatedById");
 
                     b.HasIndex("HomeLocationId");
-
-                    b.HasIndex("IdirId")
-                        .IsUnique();
-
-                    b.HasIndex("IdirName")
-                        .IsUnique();
-
-                    b.HasIndex("KeyCloakId")
-                        .IsUnique();
 
                     b.HasIndex("UpdatedById");
 
@@ -1986,7 +1963,8 @@ namespace Unified.Db.Migrations
                     b.HasOne("Unified.Db.Models.Training.TrainingCategory", "TrainingCategory")
                         .WithMany("Trainings")
                         .HasForeignKey("TrainingCategoryId")
-                        .OnDelete(DeleteBehavior.Restrict);
+                        .OnDelete(DeleteBehavior.Restrict)
+                        .IsRequired();
 
                     b.HasOne("Unified.Db.Models.UserManagement.User", "UpdatedBy")
                         .WithMany()
