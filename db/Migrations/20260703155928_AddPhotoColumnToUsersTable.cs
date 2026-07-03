9e6972460daa8a2623a249711c1446a45894daf7
Hemanth Kona <hemanth.kona@quartech.com>
2026-07-03 09:03:20 -0700

    generate migrations for adding photo for user

---
+++ db/Migrations/20260703155928_AddPhotoColumnToUsersTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unified.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoColumnToUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastPhotoUpdate",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Photo",
+                table: "Users",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastPhotoUpdate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Users");
+        }
+    }
+}
