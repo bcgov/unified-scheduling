67d8d9e53161e2303306eed73224bc5ab91d2f30
Hemanth Kona <hemanth.kona@quartech.com>
2026-07-13 10:01:48 -0400

    api(migrations): Fix region migration - set code column as nullable

---
+++ db/Migrations/20260712222015_UpdateRegionTableCodeColumnNullable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unified.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRegionTableCodeColumnNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Regions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Regions",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true
+            );
+        }
+    }
+}
