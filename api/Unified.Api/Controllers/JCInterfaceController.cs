0421e3627257a6e1cce58c0a833d861e6063b3c7
Hemanth Kona <hemanth.kona@quartech.com>
2026-07-14 08:19:31 -0400

    api(jc-interface): Setup JC Interface client
    - setup JCInterface cspropj
    - setup JCInterfaceOptions to read env variables
    - Setup JCDataUpdaterService to sync regions, locations, courtrooms to unified db
    - setup JCInterface controller to test the sync functionality
    - add FlexLabs.EntityFrameworkCore.Upsert
    - copy ListExtensons from Sheriff Scheduling

---
+++ api/Unified.Api/Controllers/JCInterfaceController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Unified.JCInterface.Services;
+
+namespace Unified.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JCInterfaceController : ControllerBase
+{
+    private readonly ILogger<JCInterfaceController> _logger;
+    private readonly JCDataUpdaterService _jcDataUpdaterService;
+
+    public JCInterfaceController(ILogger<JCInterfaceController> logger, JCDataUpdaterService jcDataUpdaterService)
+    {
+        _logger = logger;
+        _jcDataUpdaterService = jcDataUpdaterService;
+    }
+
+    [HttpPost("sync-regions")]
+    [AllowAnonymous]
+    public async Task<ActionResult> SyncRegions()
+    {
+        await _jcDataUpdaterService.SyncRegionsAsync();
+        return Ok();
+    }
+
+    [HttpPost("sync-locations")]
+    [AllowAnonymous]
+    public async Task<ActionResult> SyncLocations()
+    {
+        await _jcDataUpdaterService.SyncLocationsAsync();
+        return Ok();
+    }
+}
---
+++ api/Unified.Common/helpers/extensions/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unified.Common.Helpers.Extensions;
+
+public static class ListExtensions
+{
+    public static List<T2> SelectToList<T, T2>(this IEnumerable<T> target, Func<T, T2> lambda)
+    {
+        return target.Select(lambda).ToList();
+    }
+
+    public static List<T2> SelectDistinctToList<T, T2>(this IEnumerable<T> target, Func<T, T2> lambda)
+    {
+        return target.Select(lambda).Distinct().ToList();
+    }
+
+    public static List<T> WhereToList<T>(this IEnumerable<T> target, Func<T, bool> lambda)
+    {
+        return target.Where(lambda).ToList();
+    }
+
+    public static string ToStringWithPipes(this IEnumerable<string> contents) => string.Join("||", contents);
+}
