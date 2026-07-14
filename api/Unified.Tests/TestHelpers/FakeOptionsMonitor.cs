c78a6767d6c9dc7fcc53aa3d9f21e7a1c3dd1b7e
Hemanth Kona <hemanth.kona@quartech.com>
2026-07-14 17:00:50 -0400

    Add no op for FakeOptionsMonitor

--- api/Unified.Tests/TestHelpers/FakeOptionsMonitor.cs
+++ api/Unified.Tests/TestHelpers/FakeOptionsMonitor.cs
@@ -8,5 +8,12 @@ internal sealed class FakeOptionsMonitor<T>(T currentValue) : IOptionsMonitor<T>
 
     public T Get(string? name) => CurrentValue;
 
-    public IDisposable? OnChange(Action<T, string?> listener) => null;
+    public IDisposable OnChange(Action<T, string?> listener) => NullDisposable.Instance;
+
+    private sealed class NullDisposable : IDisposable
+    {
+        public static readonly NullDisposable Instance = new();
+
+        public void Dispose() { }
+    }
 }
