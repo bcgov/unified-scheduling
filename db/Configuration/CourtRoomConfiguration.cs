5c899402983d1f96df9d6c97d46d3f8cef7b03c2
Hemanth Kona <hemanth.kona@quartech.com>
2026-07-14 10:44:26 -0400

    Setup Court locations sync - setup CourtRooms db table - setup sync-court-rooms enpdoint for testing

---
+++ db/Configuration/CourtRoomConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Unified.Db.Models.Lookup;
+
+namespace Unified.Db.Configuration;
+
+public class CourtRoomConfiguration : BaseEntityConfiguration<CourtRoom>
+{
+    public override void Configure(EntityTypeBuilder<CourtRoom> builder)
+    {
+        builder.Property(b => b.Id).HasIdentityOptions(startValue: 1000);
+
+        builder.Property(b => b.Code).HasMaxLength(50).IsRequired();
+
+        builder.HasIndex(b => new { b.Code, b.LocationId }).IsUnique();
+
+        base.Configure(builder);
+    }
+}
