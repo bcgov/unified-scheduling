06fed08423668f4bbd9a9795d4d8d063a01ea3d0
Hemanth Kona <hemanth.kona@quartech.com>
2026-07-16 12:47:15 -0400

    Chagne CourtRoom.Code to CourtRoom.Room

--- db/Configuration/CourtRoomConfiguration.cs
+++ db/Configuration/CourtRoomConfiguration.cs
@@ -8,11 +8,9 @@ public class CourtRoomConfiguration : BaseEntityConfiguration<CourtRoom>
 {
     public override void Configure(EntityTypeBuilder<CourtRoom> builder)
     {
-        builder.Property(b => b.Id).HasIdentityOptions(startValue: 1000);
+        builder.Property(b => b.Room).HasMaxLength(50).IsRequired();
 
-        builder.Property(b => b.Code).HasMaxLength(50).IsRequired();
-
-        builder.HasIndex(b => new { b.Code, b.LocationId }).IsUnique();
+        builder.HasIndex(b => new { b.Room, b.LocationId }).IsUnique();
 
         base.Configure(builder);
     }
--- db/Models/Location/CourtRoom.cs
+++ db/Models/Location/CourtRoom.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using Mapster;
 using Unified.Db.Models.Abstract;
@@ -5,10 +6,16 @@ using Unified.Db.Models.Abstract;
 namespace Unified.Db.Models;
 
 [AdaptTo("[name]Dto")]
-public class CourtRoom : BaseCodeTypeEntity
+public class CourtRoom : BaseEntity
 {
     [Key]
     public int Id { get; set; }
+
+    public string Room { get; set; } = string.Empty;
+
+    public DateTimeOffset? EffectiveDate { get; set; }
+
+    public DateTimeOffset? ExpiryDate { get; set; }
 
     [AdaptIgnore]
     public virtual Location? Location { get; set; }
