86bc8ea80ecf86a41b676a9ead26dce407238576
Hemanth Kona <hemanth.kona@quartech.com>
2026-07-21 19:47:24 -0400

    Remove region code column

--- api/Unified.UserManagement/Models/RegionDto.cs
+++ api/Unified.UserManagement/Models/RegionDto.cs
@@ -6,7 +6,6 @@ public partial class RegionDto
 {
     public int Id { get; set; }
     public int? JustinId { get; set; }
-    public string Code { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public DateTimeOffset? ExpiryDate { get; set; }
     public uint ConcurrencyToken { get; set; }
--- db/Models/Location/Region.cs
+++ db/Models/Location/Region.cs
@@ -10,7 +10,6 @@ public class Region : BaseEntity
     [Key]
     public int Id { get; set; }
     public int? JustinId { get; set; }
-    public string? Code { get; set; }
     public string Name { get; set; }
     public DateTimeOffset? ExpiryDate { get; set; }
 }
