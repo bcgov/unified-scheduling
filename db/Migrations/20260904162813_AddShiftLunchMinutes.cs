a08aff091e55048661416664a749ed669e6db784
devinleighsmith <devinleighsmith@gmail.com>
2026-09-04 09:29:05 -0700

    add new lunch available minutes.

---
+++ db/Migrations/20260904162813_AddShiftLunchMinutes.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unified.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShiftLunchMinutes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LunchAvailableMinutes",
+                table: "ShiftSeries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkedLunchMinutes",
+                table: "ShiftSeries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LunchAvailableMinutes",
+                table: "ShiftEntries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkedLunchMinutes",
+                table: "ShiftEntries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LunchAvailableMinutes",
+                table: "ShiftSeries");
+
+            migrationBuilder.DropColumn(
+                name: "WorkedLunchMinutes",
+                table: "ShiftSeries");
+
+            migrationBuilder.DropColumn(
+                name: "LunchAvailableMinutes",
+                table: "ShiftEntries");
+
+            migrationBuilder.DropColumn(
+                name: "WorkedLunchMinutes",
+                table: "ShiftEntries");
+        }
+    }
+}
