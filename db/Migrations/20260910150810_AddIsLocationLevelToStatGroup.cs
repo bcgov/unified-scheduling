c433aea862b4f48f667af33303d55429583ff442
harmanjit.randhawa98 <harmanjit.randhawa98@gmail.com>
2026-09-10 15:13:53 +0000

    added migration

---
+++ db/Migrations/20260910150810_AddIsLocationLevelToStatGroup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unified.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsLocationLevelToStatGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocationLevel",
+                table: "StatGroups",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLocationLevel",
+                table: "StatGroups");
+        }
+    }
+}
