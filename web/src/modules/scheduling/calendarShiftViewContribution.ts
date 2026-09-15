1c7265b96126a296fc684847cbbf5a97866a170e
devinleighsmith <devinleighsmith@gmail.com>
2026-09-15 13:19:45 -0700

    mixed shift/assignment updates/cleanup bugfixes.

--- web/src/modules/scheduling/calendarShiftViewContribution.ts
+++ web/src/modules/scheduling/calendarShiftViewContribution.ts
@@ -1,12 +1,15 @@
 import CalendarSchedulingView from './CalendarSchedulingView.vue';
 import type { CalendarViewDefinition } from '@/modules/calendar/registry/calendarRegistryTypes';
 import { buildCalendarSchedulingViewModel } from './calendarSchedulingMappers.ts';
+import { canViewShifts } from './calendarSchedulingPermissions';
 
 export const calendarShiftViewContribution: CalendarViewDefinition = {
   id: 'calendar.matrix-schedule',
   label: 'Schedule View',
   order: 20,
   component: CalendarSchedulingView,
+  loadingVariant: 'matrix',
+  isAvailable: canViewShifts,
   buildModel: (data, queryContext, _runtimeContext, period) =>
     buildCalendarSchedulingViewModel(data, queryContext, period),
 };
