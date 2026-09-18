299e03d9e82ffb6928ae415bbe428a6b509970bb
JTraill <josh.traill@quartech.com>
2026-09-18 13:34:41 -0700

    refactor: add back in di

--- api/Unified.Training/TrainingModule.cs
+++ api/Unified.Training/TrainingModule.cs
@@ -56,6 +56,7 @@ public static class TrainingModule
         services.AddSingleton(TimeProvider.System);
         services.AddScoped<IUserTrainingService, UserTrainingService>();
         services.AddScoped<IUserTrainingExpiryNotificationService, UserTrainingExpiryNotificationService>();
+        services.AddScoped<IRecurringJob, UserTrainingExpiryNoticeRecurringJob>();
         services.AddScoped<ITrainingLookupStrategy, TrainingLookupStrategy>();
         services.AddScoped<IReportQueryHandler, UserTrainingReportQueryHandler>();
         services.AddScoped<ILookupStrategy>(serviceProvider =>
