aa42c482091cda50a6600412f3afff0ce49b371f
JTraill <josh.traill@quartech.com>
2026-09-24 20:46:10 -0700

    refactor: merge conflicts

--- db/Configuration/UserConfiguration.cs
+++ db/Configuration/UserConfiguration.cs
@@ -23,13 +23,6 @@ public class UserConfiguration : BaseEntityConfiguration<User>
             .IsRequired(false)
             .OnDelete(DeleteBehavior.SetNull);
 
-        builder
-            .HasOne(b => b.TrainingProfileType)
-            .WithMany(p => p.Users)
-            .HasForeignKey(b => b.TrainingProfileId)
-            .IsRequired(false)
-            .OnDelete(DeleteBehavior.SetNull);
-
         // @TODO: Enable after adding User Roles
         // builder
         //     .HasMany(m => m.UserRoles)
